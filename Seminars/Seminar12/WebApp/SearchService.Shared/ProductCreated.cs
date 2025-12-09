e09028b65b7a8a6aa4c9ff5f80dd071da9da7e2b
Jatasi98 <81189557+Jatasi98@users.noreply.github.com>
2025-12-09 23:11:27 +0100

    Feat: Seminar12 (#4)

---
+++ Seminars/Seminar12/WebApp/SearchService.Shared/ProductCreated.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchService.Shared;
+
+public sealed record ProductCreated(
+    int ProductId,
+    string Name,
+    string? Description,
+    decimal Price,
+    string? CategoryName);
---
+++ Seminars/Seminar12/WebApp/VerticalSlice/Features/Cart/AddCartItem/AddCartItemQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using VerticalSlice.DTOs;
+
+namespace VerticalSlice.Features.Cart.AddCartItem;
+
+public class AddCartItemQuery : IRequest<ProductDTO?>
+{
+    public int Id { get; set; }
+}
